7466b84eb93d4e69b8d61e4bf774e22b427bffbd
Surya Rani <srani@ucsc.edu>
2021-10-26 16:03:23 -0700

    Added html component and im displaying the correct data with html

--- crypto.go
+++ crypto.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -37,6 +37,17 @@ type Binance struct {
 	Asks         [][]string `json:"asks"`
 }
 
+type Format struct {
+	Btc1     float64
+	Btc2     float64
+	BtcSell1 float64
+	BtcSell2 float64
+	Eth1     float64
+	Eth2     float64
+	EthSell1 float64
+	EthSell2 float64
+}
+
 func main() {
 
 	http.HandleFunc("/", handler)
@@ -45,6 +56,7 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+
 	resp, err := http.Get("https://api.blockchain.com/v3/exchange/l3/BTC-USD")
 	if err != nil {
 		log.Fatalln(err)
@@ -60,7 +72,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	blockhainBtcBuy := buys.Buys[0].Price
 	blockhainBtcSell := sells.Sells[0].Price
 
-	fmt.Fprintf(w, "This is buy price from blockchain for bitcoin %f, and sell price %f\n", blockhainBtcBuy, blockhainBtcSell)
+	//fmt.Fprintf(w, "This is buy price from blockchain for bitcoin %f, and sell price %f\n", blockhainBtcBuy, blockhainBtcSell)
 
 	resp, err = http.Get("https://api.blockchain.com/v3/exchange/l3/ETH-USD")
 	if err != nil {
@@ -77,7 +89,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	blockchainEthBuy := buysEth.Buys[0].Price
 	blockchainEthSell := sellsEth.Sells[0].Price
 
-	fmt.Fprintf(w, "This is buy price from blockchain for bitcoin %f, and sell price %f\n", blockchainEthBuy, blockchainEthSell)
+	//fmt.Fprintf(w, "This is buy price from blockchain for bitcoin %f, and sell price %f\n", blockchainEthBuy, blockchainEthSell)
 
 	//getting bitcoin price from binance
 	resp, err = http.Get("https://api.binance.com/api/v3/depth?symbol=BTCUSDT")
@@ -91,7 +103,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	binanceBtcBuy, _ := strconv.ParseFloat(btcBinance.Asks[0][0], 64)
 	binanceBtcSell, _ := strconv.ParseFloat(btcBinance.Bids[0][0], 64)
-	fmt.Fprintf(w, "This is buy price from Binance for bitcoin %f, and sell price %f\n", binanceBtcBuy, binanceBtcSell)
+	//fmt.Fprintf(w, "This is buy price from Binance for bitcoin %f, and sell price %f\n", binanceBtcBuy, binanceBtcSell)
 
 	//getting ethereum price from binance
 	resp, err = http.Get("https://api.binance.com/api/v3/depth?symbol=ETHUSDT")
@@ -105,30 +117,46 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	binanceEthBuy, _ := strconv.ParseFloat(ethBinance.Asks[0][0], 64)
 	binanceEthSell, _ := strconv.ParseFloat(ethBinance.Bids[0][0], 64)
-	fmt.Fprintf(w, "This is buy price from Binance for etheruem %f, and sell price %f\n", binanceEthBuy, binanceEthSell)
+	//fmt.Fprintf(w, "This is buy price from Binance for etheruem %f, and sell price %f\n", binanceEthBuy, binanceEthSell)
 
 	if blockhainBtcBuy < binanceBtcBuy {
-		fmt.Fprintf(w, "You should buy Bitcoin from Blockchain\n")
+		//fmt.Fprintf(w, "You should buy Bitcoin from Blockchain\n")
 	} else {
-		fmt.Fprintf(w, "You should buy Bitcoin from Binance\n")
+		//fmt.Fprintf(w, "You should buy Bitcoin from Binance\n")
 	}
 
 	if blockchainEthBuy < binanceEthBuy {
-		fmt.Fprintf(w, "You should buy Ethereum from Blockchain\n")
+		//fmt.Fprintf(w, "You should buy Ethereum from Blockchain\n")
 	} else {
-		fmt.Fprintf(w, "You should buy Ethereum from Binance\n")
+		//fmt.Fprintf(w, "You should buy Ethereum from Binance\n")
 	}
 
 	if blockhainBtcSell > binanceBtcSell {
-		fmt.Fprintf(w, "You should sell Bitcoin on Blockchain\n")
+		//fmt.Fprintf(w, "You should sell Bitcoin on Blockchain\n")
 	} else {
-		fmt.Fprintf(w, "You should sell Bitcoin on Binance\n")
+		//fmt.Fprintf(w, "You should sell Bitcoin on Binance\n")
 	}
 
 	if blockchainEthSell > binanceEthSell {
-		fmt.Fprintf(w, "You should sell Ethereum on Blockchain\n")
+		//fmt.Fprintf(w, "You should sell Ethereum on Blockchain\n")
 	} else {
-		fmt.Fprintf(w, "You should sell Ethereum on Binance\n")
+		//fmt.Fprintf(w, "You should sell Ethereum on Binance\n")
 	}
+
+	templ, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	data := Format{
+		Btc1:     blockhainBtcBuy,
+		Btc2:     binanceBtcBuy,
+		BtcSell1: blockhainBtcSell,
+		BtcSell2: binanceBtcSell,
+		Eth1:     blockchainEthBuy,
+		Eth2:     binanceEthBuy,
+		EthSell1: blockchainEthSell,
+		EthSell2: binanceEthSell,
+	}
+	templ.Execute(w, data)
 
 }
